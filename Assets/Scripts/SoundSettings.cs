6cf6b4a5c097cf534964298f3fb0ceb6a0e4ba32
Muhammad Khalish Zhafran <khalish260303@gmail.com>
2022-12-07 12:05:06 +0700

    update sound

--- Assets/Scripts/SoundSettings.cs
+++ Assets/Scripts/SoundSettings.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class SoundSettings : MonoBehaviour
 {
+    public Slider soundSlider;
     public AudioMixer audioMixer;
+    Camera cam;
+
+    [SerializeField] float volume;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        cam = Camera.main;
+        GetVolume();
+    }
 
     public void SetVolume(float volume)
     {
@@ -14,6 +30,9 @@ public class SoundSettings : MonoBehaviour
 
     public void GetVolume()
     {
-        audioMixer.GetFloat("Volume", out float volume);
+        if (soundSlider != null)
+            volume = soundSlider.value;
+        // audioMixer.GetFloat("Volume", out float volume);
+        cam.GetComponent<AudioSource>().volume = volume;
     }
 }
